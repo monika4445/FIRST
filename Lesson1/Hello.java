da814577e4c740ccc4e5c6a56f21e9d3cdfe5c17
monika4445 <86877899+monika4445@users.noreply.github.com>
2021-07-05 13:10:02 +0400

    Add files via upload
    Here is my first Java project.

---
+++ Lesson1/Hello.java
@@ -0,0 +1,33 @@
+package Lesson1;
+
+public class Hello {
+    public static void main(String[] args) {
+        byte a = 16;
+        short b = -32138;
+        System.out.println(a);
+        System.out.println(b);
+        int x = 2;
+        System.out.println(x);
+        long y = 45165425L;
+        System.out.println(y);
+        System.out.println("Picsart Armenia");
+        double bb = 32.584;
+        System.out.println(bb);
+        float ff = 26.214f;
+        System.out.println(ff);
+        b = 3;
+        System.out.println(b);
+        boolean ll = false;
+        System.out.println(ll);
+        char cc = '@';
+        System.out.println(cc);
+        char yy = '\u00A7';
+        System.out.println(yy);
+        char vv = '\u038F';
+        System.out.println(vv);
+        String s = " Hello from Picsart";
+        System.out.println(s);
+        System.out.println("ff = " + ff + ", bb = " + bb);
+        System.out.println("Hello" + " My Friend");
+    }
+}
