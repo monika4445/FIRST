4705571c40056100b3d850147544461f412f13f0
monika4445 <86877899+monika4445@users.noreply.github.com>
2021-07-14 11:27:48 +0400

    Add files via upload

---
+++ Homeworks/Lesson4/TumoStudent.java
@@ -0,0 +1,22 @@
+package Homeworks.Lesson4;
+
+public class TumoStudent {
+
+    public String firstName;
+    public String lastName;
+    public int age;
+    public boolean isFromArmeniaOrArtsakh;
+    public char language;
+
+
+
+
+
+
+
+
+
+
+
+
+}
