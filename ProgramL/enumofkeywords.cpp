8e97f288d2788066e278d8c9442403299a6830ba
monika4445 <86877899+monika4445@users.noreply.github.com>
2021-11-01 19:24:49 +0400

    Add files via upload

---
+++ ProgramL/enumofkeywords.cpp
@@ -0,0 +1,85 @@
+enum struct reserved_token {
+  inc,
+  dec,
+	
+  add,
+  sub,
+  concat,
+  mul,
+  div,
+  idiv,
+  mod,
+	
+  bitwise_not,
+  bitwise_and,
+  bitwise_or,
+  bitwise_xor,
+  shiftl,
+  shiftr,
+	
+  assign,
+	
+  add_assign,
+  sub_assign,
+  concat_assign,
+  mul_assign,
+  div_assign,
+  idiv_assign,
+  mod_assign,
+
+  and_assign,
+  or_assign,
+  xor_assign,
+  shiftl_assign,
+  shiftr_assign,
+  
+  logical_not,
+  logical_and,
+  logical_or,
+  
+  eq,
+  ne,
+  lt,
+  gt,
+  le,
+  ge,
+  
+  question,
+  colon,
+  
+  comma,
+  
+  semicolon,
+  
+  open_round,
+  close_round,
+  
+  open_curly,
+  close_curly,
+  
+  open_square,
+  close_square,
+  
+  kw_if,
+  kw_else,
+  kw_elif,
+
+  kw_switch,
+  kw_case,
+  kw_default,
+
+  kw_for,
+  kw_while,
+  kw_do,
+
+  kw_break,
+  kw_continue,
+  kw_return,
+
+  kw_var,
+  kw_func,
+  
+  kw_void,
+  kw_number,
+  kw_string,
+};
