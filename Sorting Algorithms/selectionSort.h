58161563285d5f8ddee0253421e836742b55cdd6
monika4445 <86877899+monika4445@users.noreply.github.com>
2022-01-26 14:09:22 +0400

    Delete Sorting Algorithms directory

--- Sorting Algorithms/selectionSort.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef SELECTIONSORT_H
-#define SELECTIONSORT_H
-
-#include "selectionSort.cpp"
-
-void selection_sort(std::vector <int>& v1);
-#endif
--- Sorting Algorithms/swap_f.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-template <typename T>
-void swap_f(T& a, T& b) {
-	T tmp;
-
-	tmp = a;
-	a = b;
-	b = tmp;
-}
